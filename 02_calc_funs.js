365ebfc9cea155c25f3e4a3d26109bbbe8000390
satan811216 <43879360+satan811216@users.noreply.github.com>
2019-04-21 22:36:21 +0800

    00,01,02
    已經答對的檔案

--- 02_calc_funs.js
+++ 02_calc_funs.js
@@ -1,14 +1,24 @@
 // 這個函式會回傳兩個數字相加
 function add(a, b) {
   // 把你的解答寫在這裡。要記得 return 喔。
+  return a + b;
 }
 
 // 這個函式會回傳兩個數字相減
 // 注意參數的部份是不是有少什麼，補上需要的東西
-function sub() {
+function sub(a, b) {
   // 把你的解答寫在這裡。要記得 return 喔。
+  return a - b;
 }
 
 // 實作 mul 及 div 兩個函式
+function mul(a, b) {
+  // 把你的解答寫在這裡。要記得 return 喔。
+  return a * b;
+}
 
+function div(a, b) {
+  // 把你的解答寫在這裡。要記得 return 喔。
+  return a / b;
+}
 module.exports = {add, sub, mul, div};
